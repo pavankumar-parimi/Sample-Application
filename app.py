79626141bcace36f4c0800e91821f1fa27e2f4e8
pavankumar-parimi <pavankumarpev@gmail.com>
2023-09-19 09:24:55 -0500

    Modify the app.py host value

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello_world():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=4000, debug=True)
+    app.run(host='127.0.0.1', port=4000, debug=True)
