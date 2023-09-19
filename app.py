12e4fabc1700518688ee02dd4a3b00d4f51fdb49
pavankumar-parimi <pavankumarpev@gmail.com>
2023-09-19 08:50:05 -0500

    Initial Commit.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print(os.environ.get('SECRET_KEY'))
+    print(os.environ.get('MONGO_URI'))
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=4000, debug=True)
